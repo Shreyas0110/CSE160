a48d2b567d96f5285ae9dfd1bd474b7de93fbcfc
Shreyas0110 <shreyas_gupta@outlook.com>
2025-06-04 03:58:07 -0700

    heisenbug

--- asgn5/src/Objects/Player.js
+++ asgn5/src/Objects/Player.js
@@ -3,11 +3,23 @@ import { GAME } from '../gameState'
 import { GameObject } from './GameObject'
 import { inputManager } from '../inputManager';
 
+const animStatus = Object.freeze({
+    FLAT: 1,
+    TAKING_DAMAGE: 2,
+    TURNING_LEFT: 3,
+    TURNING_RIGHT: 4,
+    RESET_LEFT: 5,
+    RESET_RIGHT: 6
+   });
+
 export class Player extends GameObject{
+    
     constructor(){
         super();
         this.moveSpeed = 1.3;
-        this.animStatus
+        this.animStatus = animStatus.FLAT;
+        this.animStartTime = 0;
+        this.rotateSpeed = 20;
     }
 
     setMesh(Mesh){
@@ -16,6 +28,49 @@ export class Player extends GameObject{
         this.mesh.rotateY(THREE.MathUtils.degToRad(180));
         GAME.addToRoot(this.mesh);
         
+    }
+
+    turn(moveX, dt){
+        let currentTime = GAME.currentTime;
+        if((moveX <= 0 && this.animStatus == animStatus.TURNING_RIGHT) || (this.animStatus == animStatus.RESET_RIGHT)){
+            //console.log('e');
+            this.animStatus = animStatus.RESET_RIGHT;
+            //this.animStartTime = currentTime;
+        }
+        else if((moveX >= 0 && this.animStatus == animStatus.TURNING_LEFT) || (this.animStatus == animStatus.RESET_LEFT)){
+            this.animStatus = animStatus.RESET_LEFT;
+            //this.animStartTime = currentTime;
+        }
+        else if(moveX < 0){
+            this.animStatus = animStatus.TURNING_LEFT;
+        }
+        else if(moveX > 0){
+            this.animStatus = animStatus.TURNING_RIGHT;
+        }
+        else{
+            this.animStatus = animStatus.FLAT;
+        }
+
+        let rot = THREE.MathUtils.radToDeg(this.mesh.rotation.z);
+
+        if ((this.animStatus == animStatus.TURNING_LEFT) || (this.animStatus == animStatus.RESET_RIGHT)) {
+            this.mesh.rotation.z += (THREE.MathUtils.degToRad(-this.rotateSpeed * dt)); // rotates positive: to the right
+        } 
+        else if ((this.animStatus == animStatus.TURNING_RIGHT) || (this.animStatus == animStatus.RESET_LEFT)) {
+            this.mesh.rotation.z += (THREE.MathUtils.degToRad(this.rotateSpeed * dt)); // rotates negative: to the left
+        } 
+        rot = THREE.MathUtils.radToDeg(this.mesh.rotation.z);
+        if(rot < 0.5 && rot > -0.5){
+            this.animStatus = animStatus.FLAT;
+            this.mesh.rotation.z = 0;
+        }
+        if(rot > 60){
+            this.mesh.rotation.z = THREE.MathUtils.degToRad(60);
+        }
+        if(rot < -60){
+            this.mesh.rotation.z = THREE.MathUtils.degToRad(-60);
+        }
+        console.log(rot, this.animStatus, moveX);
     }
 
     animate(){
@@ -36,6 +91,7 @@ export class Player extends GameObject{
             moveZ += 1;
         }
         this.mesh.position.x += ds * moveX;
+        this.turn(moveX, dt);
         this.mesh.position.z += ds * moveZ;
 
         if(this.mesh.position.x < -1 * GAME.MAX_X){
